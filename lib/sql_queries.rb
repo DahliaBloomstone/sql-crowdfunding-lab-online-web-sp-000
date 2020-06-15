bb22b3be19bd690f2086959237a168a60c7c0fb2
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-15 22:25:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
     INNER JOIN pledges
     ON projects.id = pledges.project_id
     GROUP BY projects.id
-    ORDER BY projects.title;"end
+    ORDER BY projects.title;"
+  end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, pledges.amount FROM projects
